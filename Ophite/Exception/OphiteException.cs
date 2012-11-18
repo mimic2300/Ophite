196dd7ffcb862923bb319876629109dbf39148bf
amertak <kadlec.david@gmail.com>
2012-11-18 11:44:54 +0100

    Pridana zakladni Exception/OphiteException.cs pro zaobaleni vsech chyb volanych nasim kodem a poslany dale.

---
+++ Ophite/Exception/OphiteException.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Ophite.Exception
+{
+    public class OphiteException : System.Exception
+    {
+        public OphiteException()
+            : base()
+        { 
+        
+        }
+
+        public OphiteException(string message) 
+            : base(message)
+        {
+
+        }
+
+        public OphiteException(string message, System.Exception innerException)
+            : base(message, innerException)
+        { 
+        
+        }
+    }
+}
